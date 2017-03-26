cc9bf0c8b85c887809e0655e40954a86c3a0dd12
deltamaze <wcpooley@gmail.com>
2017-03-26 00:36:01 -0500

    playing with firebase function, think i got rx js timer working

--- snake.functions/functions/index.js
+++ snake.functions/functions/index.js
@@ -1,13 +1,36 @@
 var functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+var Observable = require('rxjs/Observable').Observable;
+// patch Observable with appropriate methods
+require('rxjs/add/observable/timer');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // })
-exports.startGame =functions.database.ref('/players/{pushId}/playerNum')
+exports.startGame =functions.database.ref('/players/{pushId}/')
     .onWrite(event => {
-       snap = event.data;
-      return firebase.database.;
+        // Only edit data when it is first created.
+        if (event.data.previous.exists()) {
+            return;
+        }
+        // Exit when the data is deleted.
+        if (!event.data.exists()) {
+            return;
+        }
+        const original = event.data.val();
+        var x = 1
+        admin.database().ref('/test/').set({foo:'bar'});
+        // setTimeout(function(){
+        //     admin.database().ref('/test/').set({foo:'bar2'});
+        // }, 10000);
+        // Observable.timer(3000, 1000).subscribe(x => {
+        //     admin.database().ref('/test/').set({foo:'bar'+x});
+        //     x++;
+        // });
+    
+        return;
     });
+      
