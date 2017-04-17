375c8f5d06747eb0b030429bdc8795cb658d3d4a
Wpooley <william.pooley@sunoco.com>
2017-04-17 16:20:27 -0500

    more tinkering with firebase functions and getting the param/pushid

--- snake.functions/functions/index.js
+++ snake.functions/functions/index.js
@@ -1,15 +1,12 @@
 var functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-var Observable = require('rxjs/Observable').Observable;
-// patch Observable with appropriate methods
-require('rxjs/add/observable/timer');
+//var Observable = require('rxjs/Observable').Observable;
+//// patch Observable with appropriate methods
+//require('rxjs/add/observable/timer');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+
 exports.startGame =functions.database.ref('/players/{pushId}/')
     .onWrite(event => {
         // Only edit data when it is first created.
@@ -22,8 +19,9 @@ exports.startGame =functions.database.ref('/players/{pushId}/')
         }
         var eventSnapshot = event.data;
         const original = event.data.val();
+        console.log("console log test");
         var x = 1
-        admin.database().ref('/test/').set({foo:'bar'});
+        admin.database().ref('/test/').set(event.params.pushId);
         // setTimeout(function(){
         //     admin.database().ref('/test/').set({foo:'bar2'});
         // }, 10000);
@@ -35,3 +33,18 @@ exports.startGame =functions.database.ref('/players/{pushId}/')
         return;
     }); 
       
+function Snake()
+{
+    this.vect = new Vector();
+    this.xSpeed = 0;
+    this.ySpeed= 0;
+    this.tail = []
+    this.playerNum = 0;
+    this.total = 0;
+    this.gameOver = false;
+}
+function Vector()
+{
+    this.x = 0;
+    this.y= 0;
+}
