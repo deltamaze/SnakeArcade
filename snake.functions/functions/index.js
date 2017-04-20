5affaa591d4400a3bf5faac7662ee0dd7fc45a19
deltamaze <wcpooley@gmail.com>
2017-04-19 22:51:06 -0500

    moved server side logic to node server project

--- snake.functions/functions/index.js
+++ snake.functions/functions/index.js
@@ -3,24 +3,26 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 var gameService;
 
+
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 
 exports.startGame = functions.database.ref('/players/{pushId}/')
     .onWrite(event => {
-        // Only edit data when it is first created.
-        if (event.data.previous.exists()) {
-            return;
-        }
-        // Exit when the data is deleted.
-        if (!event.data.exists()) {
-            return;
-        }
-        var eventSnapshot = event.data;
-        const original = event.data.val();
-        console.log("GameStarted for: " + event.params.pushId);
-        gameService = new GameService();
-        gameService.startGame(event.params.pushId);
+        // // Only edit data when it is first created.
+        // if (event.data.previous.exists()) {
+        //     return;
+        // }
+        // // Exit when the data is deleted.
+        // if (!event.data.exists()) {
+        //     return;
+        // }
+        // var eventSnapshot = event.data;
+        // const original = event.data.val();
+        // console.log("GameStarted for: " + event.params.pushId);
+        // gameService = new GameService();
+        // gameService.startGame(event.params.pushId);
 
         return;
     });
